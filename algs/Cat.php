9b6f758a5ed17023194ac6b57fc4043925748e25
fanghao <fanghao90s@gmail.com>
2020-07-05 21:40:51 +0800

    comments

--- algs/Cat.php
+++ algs/Cat.php
@@ -7,6 +7,20 @@ namespace Algs;
 
 class Cat
 {
+    /**
+     * % more in1.txt
+     * This is
+     *
+     * % more in2.txt
+     * a tiny
+     * test.
+     *
+     * % php Cat.php in1.txt in2.txt out.txt
+     * % more out.txt
+     * This is
+     * a tiny
+     * test.
+     */
     public static function main($args)
     {
         $out = new Out($args[count($args)-1]);
